b77e3bd623056db4fd269512ffe1b55c013bb1d7
randy-gif <76539806+randy-gif@users.noreply.github.com>
2023-02-18 20:20:24 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import {inspectionCalculator, DownTimeError, BlankError, ProductionResumedError, LastInspectionError } from "/timeCalculator.js"
 
-// set dates automatically
-let inspectionCalc = new inspectionCalculator;
-inspectionCalc.autoSetDate();
+// creates next inspection obj
+let nextInspection = new inspectionCalculator();
+// sets dates automatically
+nextInspection.autoSetDate();
+// submit button 
 document.getElementById("submitBtn").onclick = () => {
 
   // reset any errors
@@ -20,7 +22,6 @@ document.getElementById("submitBtn").onclick = () => {
   let lastInspection = convertToDateObj(lastInspectionTime,lastInspectionDate);
   let downTime = convertToDateObj(downTimeTime, downTimeDate);
   let timeResumed = convertToDateObj(timeResumedTime, timeResumedDate);
-  let nextInspection = new inspectionCalculator();
 
   // Exception handalilng
     try{
@@ -36,6 +37,15 @@ document.getElementById("submitBtn").onclick = () => {
             let productionResumedError = new ProductionResumedError("Down time is greater than production resumed");
             throw productionResumedError;
         }
+        // ask the user if they want to add more down time
+        document.getElementById("addMoreTime").style.display = "block";
+
+        // change paragraph to when next inspection is due
+        document.querySelector(".containerForNextInspection").style.display = "block";
+
+        document.getElementById("inspectionDue").innerText = 
+        nextInspection.formatTime(nextInspection.CalculateNextInspection(lastInspection, downTime, timeResumed))
+
     }catch(error){
         if(error instanceof DownTimeError){
             document.getElementById("downTimeError").style.display = "block";
@@ -53,16 +63,75 @@ document.getElementById("submitBtn").onclick = () => {
             return 0;
         }
     }
-    // ask the user if they want to add more down time
-
-
-    // change paragraph to when next inspection is due
-
-    document.getElementById("inspectionDue").innerText = 
-    nextInspection.formatTime(nextInspection.CalculateNextInspection(lastInspection, downTime, timeResumed))
  };
+ // add more downtime button
+ document.getElementById("addMoreBtn").onclick = ()=>{
 
+    // hide elemtns//
+    document.querySelector(".containerForButton").style.display = "none";
+    document.getElementById("LastInspectionContainer").style.display = "none";
+    document.getElementById("addMoreTime").style.display = "none";
+
+    // show elements//
+    document.getElementById("addMoretimeText").style.display = "block";
+    document.querySelector(".containerForButton2").style.display = "block";
+    document.querySelector(".containerForResetButton").style.display = "block";
+
+
+    //reset all times to blank//
+    document.getElementById("downTimeImpt").value = "";
+    document.getElementById("resumedImpt").value = "";
+ }
+
+ //submit button for add more down time 
+ document.getElementById("submitBtn2").onclick = () =>{
+    // reset any errors
+    resetErrors();
+
+    // Get elements and assgin them
+    let downTimeTime = document.getElementById("downTimeImpt");
+    let timeResumedTime = document.getElementById("resumedImpt");
+    let downTimeDate = document.getElementById("donwTimeDate");
+    let timeResumedDate = document.getElementById("timeResumedDate");
+
+    // convert elements to Date objects
+    let downTime = convertToDateObj(downTimeTime, downTimeDate);
+    let timeResumed = convertToDateObj(timeResumedTime, timeResumedDate);
+
+    // Exception handalilng
+    try{
+        if ( downTime.getTime() > timeResumed.getTime()){
+            let productionResumedError = new ProductionResumedError("Down time is greater than production resumed");
+            throw productionResumedError;
+        }
+        else if(downTime.getTime() >= nextInspection.getTime()){
+            let downTimeError = new DownTimeError("Down time needs to be less than Next inspection due");
+            throw downTimeError;
+        }
+
+        // change paragraph to when next inspection is due
+        document.querySelector(".containerForNextInspection").style.display = "block";
+
+        document.getElementById("inspectionDue").innerText = 
+        nextInspection.formatTime(nextInspection.AddMoreDownTime(downTime, timeResumed))
+
+    }catch(error){
+        if(error instanceof ProductionResumedError){
+            document.getElementById("ProductionResumedError").style.display = "block";
+            document.getElementById("ProductionResumedError").innerText =`${error.name}: ${error.message}`;
+            return 0;
+        }
+        else if(error instanceof DownTimeError){
+            document.getElementById("downTimeError").style.display = "block";
+            document.getElementById("downTimeError").innerText =`${error.name}: ${error.message}`;
+            return 0;
+        }
+    }
+ }
  
+ document.getElementById("resetButton").onclick = () =>{
+    location.reload();
+ }
 
  function convertToDateObj(timeStr, dateStr){
     let hourStr = timeStr.value[0] + timeStr.value[1];
@@ -80,6 +149,8 @@ document.getElementById("submitBtn").onclick = () => {
         if(error instanceof BlankError){
             document.getElementById("BlankError").style.display = "block";
             document.getElementById("BlankError").innerText =`${error.name}: ${error.message}`;
+            document.getElementById("BlankError2").style.display = "block";
+            document.getElementById("BlankError2").innerText =`${error.name}: ${error.message}`;
             return 0;
         }
     }
@@ -99,6 +170,7 @@ document.getElementById("submitBtn").onclick = () => {
     document.getElementById("downTimeError").style.display = "none";
     document.getElementById("ProductionResumedError").style.display = "none";
     document.getElementById("BlankError").style.display = "none";
+    document.getElementById("BlankError2").style.display = "none";
     document.getElementById("lastInspectionError").style.display = "none";
  }
 
